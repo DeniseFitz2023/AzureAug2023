d2bb3eb82d9ee908d1d5535573926a1238358722
Denise Azure <Denise.Poll3@gmail.com>
2023-08-08 13:36:43 -0400

    added another word to App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   const value = 'World';
   const value2 = 'Aug 9th'
-  return <div>Hello {value} {value2} </div>;
+  return <div>Hello ZZZ {value} {value2} </div>;
 }
 
 export default App;
