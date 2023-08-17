adc1c7989d8281ab9f4c6c77f512bf56c8886aa3
Denise Poll <DeniseFitzSoftware@gmail.com>
2023-08-17 11:13:40 -0400

    Misc changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   const value = 'World';
   const value2 = 'Aug 9th'
-  return <div>Hello ZZZ {value} {value2} </div>;
+  return <div>Hello aug17 {value} {value2} </div>;
 }
 
 export default App;
